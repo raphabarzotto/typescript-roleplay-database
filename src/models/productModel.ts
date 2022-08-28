ad032efa5e83fbcb8a0ce78cd476721fc02da322
raphabarzotto <raphabarzotto@gmail.com>
2022-08-28 19:33:26 -0300

    fix: lint

--- src/models/productModel.ts
+++ src/models/productModel.ts
@@ -1,13 +1,13 @@
-import connection from "./connection";
-import { IProduct, IProductWithID } from "../interfaces/productInterface";
 import { ResultSetHeader } from 'mysql2';
+import connection from './connection';
+import { IProduct, IProductWithID } from '../interfaces/productInterface';
 
 export async function create(productToCreate: IProduct): Promise<IProductWithID> {
   const { name, amount } = productToCreate;
-  const query = "INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)"
+  const query = 'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)';
 
   const [result] = await connection.execute<ResultSetHeader>(query, [name, amount]);
   const { insertId } = result;
 
-  return { ...productToCreate, id: insertId}
+  return { ...productToCreate, id: insertId };
 }
