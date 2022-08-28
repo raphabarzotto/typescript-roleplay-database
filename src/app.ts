9d8e0cc5487c653f8a3bd719e65f4918bead8063
raphabarzotto <raphabarzotto@gmail.com>
2022-08-28 19:27:23 -0300

    feat: add route to app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 
+import productRouter from './routes/productRoutes';
+
 const app = express();
 
 app.use(express.json());
+app.use('/product', productRouter);
 
 export default app;
