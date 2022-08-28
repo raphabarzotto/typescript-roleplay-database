28abf49a93dd93885b831bd30e876698ef671c85
raphabarzotto <raphabarzotto@gmail.com>
2022-08-28 19:38:32 -0300

    feat: add getAll to product controller

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -7,3 +7,8 @@ export async function create(req: Request, res: Response) {
   const { status, data } = await productService.create(product);
   res.status(status).json(data);
 }
+
+export async function getAll(_req: Request, res: Response) {
+  const { status, data } = await productService.getAll();
+  res.status(status).json(data);
+}
