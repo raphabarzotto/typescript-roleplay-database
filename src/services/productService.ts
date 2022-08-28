a67ded48f4f8b69d0d88ee842f5d6398558cb370
raphabarzotto <raphabarzotto@gmail.com>
2022-08-28 19:37:58 -0300

    feat: add getAll to product service

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -5,3 +5,8 @@ export async function create(product: IProduct) {
   const data = await productModel.create(product);
   return { status: 201, data };
 }
+
+export async function getAll() {
+  const data = await productModel.getAll();
+  return { status: 200, data };
+}
