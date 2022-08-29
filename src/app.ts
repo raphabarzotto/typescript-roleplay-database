81aef4975ff4b250323b6eb81effdc7eff5c7948
raphabarzotto <raphabarzotto@gmail.com>
2022-08-29 00:36:28 -0300

    feat: add /login route to app

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import productsRouter from './routes/productsRoutes';
 import usersRouter from './routes/usersRoutes';
 import ordersRouter from './routes/ordersRoutes';
+import loginRouter from './routes/loginRoutes';
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.use(express.json());
 app.use('/products', productsRouter);
 app.use('/users', usersRouter);
 app.use('/orders', ordersRouter);
+app.use('/login', loginRouter);
 
 export default app;
