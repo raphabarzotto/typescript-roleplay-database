6f1d0e13620a0b316dbb18178ef5210984a14a54
raphabarzotto <raphabarzotto@gmail.com>
2022-08-28 21:57:21 -0300

    chore: change function names

--- src/services/usersService.ts
+++ src/services/usersService.ts
@@ -1,7 +1,7 @@
 import { IUser } from '../interfaces/usersInterface';
 import createUser from '../models/usersModel';
 
-export default async function create(user: IUser) {
+export default async function createUserService(user: IUser) {
   const data = await createUser(user);
   return { status: 201, data };
 }
