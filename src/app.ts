2954eefb6358c8b7873067a6ef38d4f4edbe89ac
raphabarzotto <raphabarzotto@gmail.com>
2022-08-28 23:34:32 -0300

    feat: add /orders router to app

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express from 'express';
 
 import productsRouter from './routes/productsRoutes';
 import usersRouter from './routes/usersRoutes';
+import ordersRouter from './routes/ordersRoutes';
 
 const app = express();
 
 app.use(express.json());
 app.use('/products', productsRouter);
 app.use('/users', usersRouter);
+app.use('/orders', ordersRouter);
 
 export default app;
