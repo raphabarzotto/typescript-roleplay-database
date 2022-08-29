751eec34e8ffd7eb10d7633f7a5f0a7c687dab9a
raphabarzotto <raphabarzotto@gmail.com>
2022-08-29 01:58:19 -0300

    feat: add orders post service

--- src/services/ordersService.ts
+++ src/services/ordersService.ts
@@ -1,6 +1,14 @@
-import getAllOrdersModel from '../models/ordersModel';
+import * as ordersModel from '../models/ordersModel';
+import { IOrder } from '../interfaces/ordersInterface';
 
-export default async function getAllOrdersService() {
-  const data = await getAllOrdersModel();
+export async function getAllOrdersService() {
+  const data = await ordersModel.getAllOrdersModel();
   return { status: 200, data };
+}
+
+export async function postOrderService(body: IOrder) {
+  const { userId, productsIds } = body;
+
+  const result = await ordersModel.postOrderModel({ userId, productsIds });
+  return { status: 201, message: result };
 }
