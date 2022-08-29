43451603532938e1ea091aee2b8d332d3fa7c632
raphabarzotto <raphabarzotto@gmail.com>
2022-08-29 01:08:32 -0300

    feat: add users validation middlewares routes

--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -1,8 +1,16 @@
 import { Router } from 'express';
 import createUserController from '../controllers/userController';
+import usersMiddlewares from '../middlewares/usersMiddlewares';
 
 const usersRouter = Router();
 
-usersRouter.post('/', createUserController);
+usersRouter.post(
+  '/',
+  usersMiddlewares.nameValidation,
+  usersMiddlewares.claseValidation,
+  usersMiddlewares.levelValidation,
+  usersMiddlewares.passwordValidation,
+  createUserController,
+);
 
 export default usersRouter;
