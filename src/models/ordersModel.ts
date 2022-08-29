c09b0c224db4fa783b8c0843b1d081ab481bbd9b
raphabarzotto <raphabarzotto@gmail.com>
2022-08-28 23:46:37 -0300

    fix: orders model query - https://www.tutorialspoint.com/mysql/mysql_aggregate_functions_json_arraygg.htm

--- src/models/ordersModel.ts
+++ src/models/ordersModel.ts
@@ -2,20 +2,22 @@ import connection from './connection';
 import { IOrder } from '../interfaces/ordersInterface';
 
 export default async function getAllOrdersModel(): Promise<IOrder[]> {
+  // source: https://www.tutorialspoint.com/mysql/mysql_aggregate_functions_json_arraygg.htm
   const query = `
     SELECT
       Orders.id,
-      userId,
-      Products.id AS productsIds
+      Orders.userId,
+      JSON_ARRAYAGG(Products.id) AS productsIds
     FROM
       Trybesmith.Orders
     INNER JOIN
       Trybesmith.Products
-        ON Products.orderId = Orders.id
+    ON Products.orderId = Orders.id
     GROUP BY Orders.id
     ORDER BY Orders.userid;`;
 
   const [result] = await connection.execute(query);
+
 
   return result as IOrder[];
 }
