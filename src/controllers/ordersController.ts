8c532000d39284660299c36647d790e3792036f2
raphabarzotto <raphabarzotto@gmail.com>
2022-08-28 23:53:38 -0300

    fix: imports

--- src/controllers/ordersController.ts
+++ src/controllers/ordersController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { getAllOrdersService } from '../services/ordersService';
+import getAllOrdersService from '../services/ordersService';
 
 export default async function getAllOrdersController(_req: Request, res: Response) {
   const { status, data } = await getAllOrdersService();
