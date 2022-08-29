9320845ad2a1647581c01318022aa13ce6bf0fc9
raphabarzotto <raphabarzotto@gmail.com>
2022-08-28 22:02:12 -0300

    feat: add jwt token generator

---
+++ src/helpers/jwtGenerator.ts
@@ -0,0 +1,10 @@
+import { sign } from 'jsonwebtoken';
+import { IToken } from '../interfaces/tokenInterface';
+
+const jwtConfig = {
+  expiresIn: '1d',
+}; 
+
+const SECRET = process.env.JWT_SECRET || 'Trybesmith';
+
+export default (data: IToken) => sign({ data }, SECRET, jwtConfig);
