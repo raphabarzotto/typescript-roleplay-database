b3ba5bb94f6056d1b11d021aeea6a81b60c0198f
raphabarzotto <raphabarzotto@gmail.com>
2022-08-28 22:55:07 -0300

    feat: add /users route in app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 
 import productsRouter from './routes/productsRoutes';
+import usersRouter from './routes/userRoutes';
 
 const app = express();
 
 app.use(express.json());
 app.use('/products', productsRouter);
+app.use('/users', usersRouter);
 
 export default app;
