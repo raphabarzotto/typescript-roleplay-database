20a07999cdfeb68d21fb14f2dd60cb47dde8104f
raphabarzotto <raphabarzotto@gmail.com>
2022-08-29 00:16:11 -0300

    feat: add login model

---
+++ src/models/loginModel.ts
@@ -0,0 +1,21 @@
+import connection from './connection';
+import { ILogin } from '../interfaces/loginInterface';
+
+export default async function login(body: ILogin): Promise<ILogin[]> {
+  const { username, password } = body;
+  const query = `
+    SELECT
+      id,
+      username,
+      password
+    FROM
+      Trybesmith.Users as users
+    WHERE
+      users.username = ? 
+    AND
+      users.password = ? `;
+
+  const [result] = await connection.execute(query, [username, password]);
+
+  return result as ILogin[];
+}
